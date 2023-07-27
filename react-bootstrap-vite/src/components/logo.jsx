34a80892bf817be658dcd1800f6ee6ae0560e519
donny_444 <104888835+donny444@users.noreply.github.com>
2023-07-27 16:05:48 +0700

    Add files via upload

---
+++ react-bootstrap-vite/src/components/logo.jsx
@@ -0,0 +1,11 @@
+function Logo() {
+    return (
+        <div className="text-center d-inline">
+            <img src="/images/react.png" alt="react logo" />
+            <img src="/images/bootstrap.png" alt="bootstrap logo" />
+            <img src="/images/vitejs.png" alt="vitejs logo" />
+        </div>
+    )
+}
+
+export default Logo
--- react-bootstrap-vite/src/components/section.jsx
+++ react-bootstrap-vite/src/components/section.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
+import Logo from "./logo.jsx"
 
 function Section() {
     const [count, setCount] = useState(0)
 
     return (
         <div>
+            <Logo />
             <div className="mx-5">
                 <h2>Lorem ipsum dolor sit amet.</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint praesentium, fuga illum consectetur at, vero dolore, explicabo earum quaerat sit eos nesciunt officia nulla! Impedit dolore rerum exercitationem tempore ipsam odio, beatae aut consequatur eum!</p>
