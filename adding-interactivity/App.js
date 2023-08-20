4e290a55d741d19cfcb934b6517c4294dd216027
donny_444 <104888835+donny444@users.noreply.github.com>
2023-08-20 22:44:45 +0700

    Add files via upload

---
+++ adding-interactivity/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import SocialBar from './components/social.js';
+import BigTechBar from './components/bigtech.js';
+import PrimeMinistersList from './components/primeministers';
+
+function App() {
+  return (
+    <>
+    <SocialBar />
+    <BigTechBar />
+    <PrimeMinistersList />
+    </>
+  );
+}
+
+export default App;
